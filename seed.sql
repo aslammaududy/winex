21af452f33df98ead4c0bbf3afcebe5894501020
aslammaududy <forjob.aslam@gmail.com>
2025-06-15 17:10:31 +0700

    add seed data

---
+++ seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO folders (id, name, parent_id)
+VALUES (1, 'Root', NULL),
+       (2, 'Documents', 1),
+       (3, 'Reports', 2),
+       (4, 'Photos', 1),
+       (5, 'Vacations', 4),
+       (6, '2023', 5),
+       (7, 'Work', 1);
